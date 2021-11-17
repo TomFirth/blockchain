91f3dc019ee54a273f019196bb184797b7c84483
TomFirth <tom.firth84@gmail.com>
2021-11-17 12:57:27 +0000

    updates

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const SHA256 = require('crypto-js/sha256')
 
 class Block {
-  constructor (timestamp, data) {
-    this.index = 0
+  constructor (index, timestamp, data, previousHash = '') {
+    this.index = index
     this.timestamp = timestamp
     this.data = data
-    this.previousHash = '0'
+    this.previousHash = previousHash
     this.hash = this.calculateHash()
     this.nonce = 0
   }
 
   calculateHash () {
-    return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString()
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
   }
 
-  mineBlock (difficulty) {
-
+  mineBlock(difficulty) {
+    while(this.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++
+      this.hash = this.calculateHash()
+    }
+    console.log("Block mined: ", this.hash)
   }
 }
 
@@ -34,7 +38,7 @@ class Blockchain {
 
   addBlock (newBlock) {
     newBlock.previousHash = this.latestBlock().hash
-    newBlock.hash = newBlock.calculateHash()
+    newBlock.mineBlock(this.difficulty)
     this.chain.push(newBlock)
   }
 
@@ -42,23 +46,16 @@ class Blockchain {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i]
       const previousBlock = this.chain[i - 1]
-
-      if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false
-      }
+      if (currentBlock.hash !== currentBlock.calculateHash()
+        || currentBlock.previousHash !== previousBlock.hash) return false
     }
-
     return true
   }
 }
 
-let bc = new Blockchain()
-bc.addBlock(new Block('12/25/2017', {amount: 5}))
-bc.addBlock(new Block('12/26/2017', {amount: 10}))
+// let beardCoin = new Blockchain()
+// beardCoin.addBlock(new Block('12/25/2017', {amount: 5}))
+// beardCoin.addBlock(new Block('12/26/2017', {amount: 10}))
 
-console.log(JSON.stringify(bc, null, 4))
-console.log('Is blockchain valid?', bc.checkValid())
+// console.log(JSON.stringify(beardCoin, null, 4))
+// console.log('Is blockchain valid?', beardCoin.checkValid())
